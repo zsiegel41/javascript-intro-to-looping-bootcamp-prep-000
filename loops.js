332fb4ee3182d313649f2735fc54d41e184fdbc5
Zach Siegel <zachsiegel41@gmail.com>
2017-09-16 01:35:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,8 +18,9 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
+  return Math.random() >= 0.5
   do {
-return Math.random() >= 0.5
+array.pop()
   } while (array.length > 0 && maybeTrue());
 return array
 }
