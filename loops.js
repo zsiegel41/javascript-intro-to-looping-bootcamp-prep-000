7ee690a4511dcea9839f34f51d5737b88e5f0ede
Zach Siegel <zachsiegel41@gmail.com>
2017-09-16 01:37:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ for (let i = 0; i < 25; i++) {
     return "I am 1 strange loop."
   }
   else {
-    return "I am ${i} strange loops${i === 0 ? '' : 's'}."
+    return "I am ${i} strange loops."
   }
 }
 return array
